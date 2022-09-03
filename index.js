c086e96942b7a7d09b36f7294b58aa505ae13c29
BhagyashreeAtre <bhagyashriatre08@gmail.com>
2022-09-03 11:22:04 +0530

    Added js file

---
+++ index.js
@@ -0,0 +1,32 @@
+const monthEl=document.querySelector(".date h1");
+const fullDateEl=document.querySelector(".date p");
+const daysEl=document.querySelector(".days");
+
+const monthInx=new Date().getMonth();
+const lastDay=new Date(new Date().getFullYear(),monthInx+1,0).getDate();
+const FirstDay=new Date(new Date().getFullYear(),monthInx,1).getDay()-1;
+
+
+const months=[
+    "January","February" ,"March", "April", "May", "June", "July", "August", "September", "October","November","December",
+];
+console.log(months[monthInx]);
+
+monthEl.inneText=months[monthInx];
+fullDateEl.inneText=new Date().toDateString();
+
+let days="";
+
+for(let i=FirstDay;i>0;i--){
+    days += `<divclass="empty" ></divclass=>`;
+}
+
+for(let i=1;i<=lastDay;i++){
+    if(i=new Date().getDate()){
+        days += `<div class="today">${i}</div>`;
+    }else{
+        days += `<div>${i}</div>`;
+    }
+   
+}
+daysEl.innerHTML= days;
